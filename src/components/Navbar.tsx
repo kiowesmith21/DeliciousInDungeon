a4e4afe2c582af6bc01914a3726f1d4828089e80
unknown <kiowesmith@gmail.com>
2024-03-11 14:49:59 -0400

    start homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+<>
+    <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 ">
+        <div className="max-w-screen-xl  justify-between flex flex-wrap items-center mx-auto p-4">
+            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap ">Flowbite</span>
+            </a>
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 " aria-current="page">Characters</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Monsters</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Recipes</a>
+                </li>
+                <li>
+                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 ">Games</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+<>
+  <div className='flex flex-col'>
+    <div className="w-full h-auto mt-16">
+      <img src="https://images4.alphacoders.com/134/1348242.jpeg" alt="" className="w-full h-auto"/>
+      <div className="absolute inset-0 flex flex-col ml-36 mt-72 w-1/2">
+        <h1 className="text-white text-7xl font-bold">Delicious in Dungeon</h1>
+        <p className="text-white text-4xl font-bold py-5">One sentence hook, get ready for an adventure!</p>
+        <a href="#trailer">
+          <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 w-1/6">Watch Trailer</button>
+        </a>
+      </div>
+    </div>
+
+    <div className='w-full h-auto max-w-full flex flex-row align-middle justify-center md:h-[480px] py-10' id="trailer">
+    <iframe width="928" height="522" src="https://www.youtube.com/embed/aXfVHMILKJg" title="Delicious in Dungeon | Official Trailer 2 | Netflix" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+
+    <div className='w-full h-auto max-w-full flex flex-row'>
+      <img src="https://static.zerochan.net/Dungeon.Meshi.1024.3971909.webp" alt="" className='w-1/6 h-auto'/>
+      <div className='flex flex-col'>
+        <h1>Embrace Food!</h1>
+        <p>bunch of nonsense</p>
+      </div>
+    </div>
+  </div>
+</>
   )
 }
+
+
 
 export default Home
