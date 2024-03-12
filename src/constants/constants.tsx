2a7039adb93fdba7a449045067cb24562d53e0a5
unknown <kiowesmith@gmail.com>
2024-03-12 17:41:25 -0400

    create context and complete dish page

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -4,7 +4,7 @@ const DISHES = {
 
         'DESCRIPTION': "A dish prepared by Senshi in collaboration with Chilchuck, using Senshi's cooking prowess and Chilchuck's skill with traps. Shredded mandrake and marinated bat breast are coated in batter and deep fried in olive oil, tempura-style.",
         
-        'RECIPE': "Mandrake (3 med. size) Big Bat breast meat (400 grams) Basilisk egg (1) Weak flour (150 grams) Water (170 cc) Soy sauce (1 tbsp) Sake (1 tbsp) Salt (a pinch) Garlic & ginger (to taste)",
+        'RECIPE': "Mandrake (3 med. size), Big Bat breast meat (400 grams), Basilisk egg (1), Weak flour (150 grams), Water (170 cc), Soy sauce (1 tbsp), Sake (1 tbsp), Salt (a pinch), Garlic & ginger (to taste)",
 
         'IMAGE': "https://cdn.vox-cdn.com/thumbor/XaArk6R5WZIC8XFFe81GeG_IrIQ=/0x0:1000x563/1720x0/filters:focal(0x0:1000x563):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/25250562/3_1_Mandrake_Kakiage___Giant_Bat_Tempura__Anime_.jpeg"
      },
