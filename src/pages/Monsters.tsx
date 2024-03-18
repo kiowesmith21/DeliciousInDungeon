49e40fa02f59f783e6a8b94f0708fe4efe8cb934
unknown <kiowesmith@gmail.com>
2024-03-18 19:16:07 -0400

    add constants and text

--- src/pages/Monsters.tsx
+++ src/pages/Monsters.tsx
@@ -28,9 +28,46 @@ const Monsters = () => {
         <div className='flex flex-col items-center md:w-1/4'>
             <h1 className='text-3xl font-bold pb-5'>{MONSTERS.BASILISK.NAME}</h1>
             <p className='font-medium text-xl pb-5'>{MONSTERS.BASILISK.DESCRIPTION}</p>
-            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.ROASTBASILISK.NAME, RECIPES.ROASTBASILISK.DESCRIPTION, RECIPES.ROASTBASILISK.RECIPE, RECIPES.ROASTBASILISK.IMAGE)}>{RECIPES.ROASTBASILISK.NAME}</Link>
+            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.ROASTBASILISK.NAME, RECIPES.ROASTBASILISK.DESCRIPTION, RECIPES.ROASTBASILISK.RECIPE, RECIPES.ROASTBASILISK.IMAGE)}>{RECIPES.ROASTBASILISK.NAME},</Link>
+            <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.BASILISKOMELET.NAME, RECIPES.BASILISKOMELET.DESCRIPTION, RECIPES.BASILISKOMELET.RECIPE, RECIPES.BASILISKOMELET.IMAGE)}>{RECIPES.BASILISKOMELET.NAME}</Link>
         </div>
         <img src={MONSTERS.BASILISK.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+    </div>
+    <div className='max-w-full flex flex-col md:flex-row items-center justify-evenly p-10'>
+        <img src={MONSTERS.HUGESCORPION.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{MONSTERS.HUGESCORPION.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{MONSTERS.HUGESCORPION.DESCRIPTION}</p>
+            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.SCORPIONHOTPOT.NAME, RECIPES.SCORPIONHOTPOT.DESCRIPTION, RECIPES.SCORPIONHOTPOT.RECIPE, RECIPES.SCORPIONHOTPOT.IMAGE)}>{RECIPES.SCORPIONHOTPOT.NAME}</Link>
+        </div>
+    </div>
+    <div className='max-w-full flex flex-col-reverse md:flex-row items-center justify-evenly p-10'>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{MONSTERS.WALKINGMUSHROOM.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{MONSTERS.WALKINGMUSHROOM.DESCRIPTION}</p>
+            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.SCORPIONHOTPOT.NAME, RECIPES.SCORPIONHOTPOT.DESCRIPTION, RECIPES.SCORPIONHOTPOT.RECIPE, RECIPES.SCORPIONHOTPOT.IMAGE)}>{RECIPES.SCORPIONHOTPOT.NAME}</Link>
+        </div>
+        <img src={MONSTERS.WALKINGMUSHROOM.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+    </div>
+    <div className='max-w-full flex flex-col md:flex-row items-center justify-evenly p-10'>
+        <img src={MONSTERS.LIVINGARMOR.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{MONSTERS.LIVINGARMOR.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{MONSTERS.LIVINGARMOR.DESCRIPTION}</p>
+            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.LIVINGARMORSTIRFRY.NAME, RECIPES.LIVINGARMORSTIRFRY.DESCRIPTION, RECIPES.LIVINGARMORSTIRFRY.RECIPE, RECIPES.LIVINGARMORSTIRFRY.IMAGE)}>{RECIPES.LIVINGARMORSTIRFRY.NAME}</Link>
+        </div>
+    </div>
+    <div className='max-w-full flex flex-col-reverse md:flex-row items-center justify-evenly p-10'>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{MONSTERS.MIMIC.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{MONSTERS.MIMIC.DESCRIPTION}</p>
+            <p className='font-medium text-xl underline'>Used In: </p> <Link to="/dish" className='font-medium text-xl hover:font-bold' onClick={() => handleClick(RECIPES.BOILEDMIMIC.NAME, RECIPES.BOILEDMIMIC.DESCRIPTION, RECIPES.BOILEDMIMIC.RECIPE, RECIPES.BOILEDMIMIC.IMAGE)}>{RECIPES.BOILEDMIMIC.NAME}</Link>
+        </div>
+        <img src={MONSTERS.MIMIC.IMAGE} alt="" 
         className='border-4 border-dungeon-dark w-1/2'/>
     </div>
 
--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -39,6 +39,54 @@ const Recipes = () => {
         className='border-4 border-dungeon-dark w-1/2'/>
     </div>
 
+    <div className='max-w-full flex flex-col md:flex-row items-center justify-evenly p-10'>
+        <img src={RECIPES.BASILISKOMELET.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{RECIPES.BASILISKOMELET.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{RECIPES.BASILISKOMELET.DESCRIPTION}</p>
+            <Link to="/dish" className='font-medium text-xl hover:font-bold'>
+              <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-full text-sm px-5 py-2.5 me-2 mb-2" onClick={() => handleClick(RECIPES.BASILISKOMELET.NAME, RECIPES.BASILISKOMELET.DESCRIPTION, RECIPES.BASILISKOMELET.RECIPE, RECIPES.BASILISKOMELET.IMAGE)}>View Recipe</button>
+            </Link>
+        </div>
+    </div>
+
+    <div className='max-w-full flex flex-col-reverse md:flex-row items-center justify-evenly p-10'>
+        <div className='flex flex-col items-center md:w-1/4'>
+          <h1 className='text-3xl font-bold pb-5'>{RECIPES.SCORPIONHOTPOT.NAME}</h1>
+          <p className='font-medium text-xl pb-5'>{RECIPES.SCORPIONHOTPOT.DESCRIPTION}</p>
+          <Link to="/dish" className='font-medium text-xl hover:font-bold'>
+            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-full text-sm px-5 py-2.5 me-2 mb-2" onClick={() => handleClick(RECIPES.SCORPIONHOTPOT.NAME, RECIPES.SCORPIONHOTPOT.DESCRIPTION, RECIPES.SCORPIONHOTPOT.RECIPE, RECIPES.SCORPIONHOTPOT.IMAGE)}>View Recipe</button>
+          </Link>
+        </div>
+        <img src={RECIPES.SCORPIONHOTPOT.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+    </div>
+
+    <div className='max-w-full flex flex-col md:flex-row items-center justify-evenly p-10'>
+        <img src={RECIPES.LIVINGARMORSTIRFRY.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+        <div className='flex flex-col items-center md:w-1/4'>
+            <h1 className='text-3xl font-bold pb-5'>{RECIPES.LIVINGARMORSTIRFRY.NAME}</h1>
+            <p className='font-medium text-xl pb-5'>{RECIPES.LIVINGARMORSTIRFRY.DESCRIPTION}</p>
+            <Link to="/dish" className='font-medium text-xl hover:font-bold'>
+              <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-full text-sm px-5 py-2.5 me-2 mb-2" onClick={() => handleClick(RECIPES.LIVINGARMORSTIRFRY.NAME, RECIPES.LIVINGARMORSTIRFRY.DESCRIPTION, RECIPES.LIVINGARMORSTIRFRY.RECIPE, RECIPES.LIVINGARMORSTIRFRY.IMAGE)}>View Recipe</button>
+            </Link>
+        </div>
+    </div>
+
+    <div className='max-w-full flex flex-col-reverse md:flex-row items-center justify-evenly p-10'>
+        <div className='flex flex-col items-center md:w-1/4'>
+          <h1 className='text-3xl font-bold pb-5'>{RECIPES.BOILEDMIMIC.NAME}</h1>
+          <p className='font-medium text-xl pb-5'>{RECIPES.BOILEDMIMIC.DESCRIPTION}</p>
+          <Link to="/dish" className='font-medium text-xl hover:font-bold'>
+            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-full text-sm px-5 py-2.5 me-2 mb-2" onClick={() => handleClick(RECIPES.BOILEDMIMIC.NAME, RECIPES.BOILEDMIMIC.DESCRIPTION, RECIPES.BOILEDMIMIC.RECIPE, RECIPES.BOILEDMIMIC.IMAGE)}>View Recipe</button>
+          </Link>
+        </div>
+        <img src={RECIPES.BOILEDMIMIC.IMAGE} alt="" 
+        className='border-4 border-dungeon-dark w-1/2'/>
+    </div>
+
   </div>
 </>
   )
